3ad80f2cda7a45aa6367f6ab21ced496cdb5b276
widadmogral@gmail.com <widadmogral@gmail.com>
2022-10-12 13:35:24 +0200

    adding weather

--- welcomescript.sh
+++ welcomescript.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/zsh
 
-echo "Welcome $(whoami)!"
+echo "Welcome $(whoami)! \n"
 
-date '+Today is %d/%m/%y, time now is %H:%M'
+date '+Today is %d/%m/%y, Time now is %H:%M'
 
-echo "“Life is full of surprises, but never when you need one.” – Calvin"
+echo "\n"
+
+echo "“Life is full of surprises, but never when you need one.” – Calvin \n"
+
+echo "Remember - Es gibt kein schlechtes Wetter, nur schlechte Kleidung \n  "
+
+curl wttr.in
